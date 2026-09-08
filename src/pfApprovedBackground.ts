11572b7c67a99759bac7d0d5cf37f90dfaf1dea5
Matt1981g <122229650+Matt1981g@users.noreply.github.com>
2026-09-08 21:50:55 +0100

    Assemble approved PF background asset

---
+++ src/pfApprovedBackground.ts
@@ -0,0 +1,11 @@
+import p0 from './assets/pf-bg-approved-1024-0.txt?raw'
+import p1 from './assets/pf-bg-approved-1024-1.txt?raw'
+import p2 from './assets/pf-bg-approved-1024-2.txt?raw'
+import p3 from './assets/pf-bg-approved-1024-3.txt?raw'
+import p4 from './assets/pf-bg-approved-1024-4.txt?raw'
+import p5 from './assets/pf-bg-approved-1024-5.txt?raw'
+import p6 from './assets/pf-bg-approved-1024-6.txt?raw'
+import p7 from './assets/pf-bg-approved-1024-7.txt?raw'
+
+export const PF_APPROVED_BACKGROUND =
+  `data:image/webp;base64,${p0}${p1}${p2}${p3}${p4}${p5}${p6}${p7}`
