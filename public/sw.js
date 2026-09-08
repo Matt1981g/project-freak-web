c194b200b6a6bbdbf64622cd409dc5dc0188de09
Matt1981g <122229650+Matt1981g@users.noreply.github.com>
2026-09-08 21:00:23 +0100

    Bust PWA cache for plate reference theme

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'project-freak-shell-v6-vintage-iron'
+const CACHE_NAME = 'project-freak-shell-v7-plate-reference'
 
 function app_url(path = '') {
   return new URL(path, self.registration.scope).href
