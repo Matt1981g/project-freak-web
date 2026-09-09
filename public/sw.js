e92e2ced8c5509ae8a0d6131022563a2601b8311
Matt1981g <122229650+Matt1981g@users.noreply.github.com>
2026-09-09 18:43:34 +0100

    Bump PWA cache for approved PF background

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'project-freak-shell-v10-approved-bg'
+const CACHE_NAME = 'project-freak-shell-v11-approved-bg-live'
 
 function app_url(path = '') {
   return new URL(path, self.registration.scope).href
